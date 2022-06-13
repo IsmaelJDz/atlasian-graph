d15c16f9313328ec5b91db04da169cbcbc6a9cdf
Ismael Barrios R <ismael_br7@hotmail.com>
2022-06-13 15:36:28 -0500

    [Finished] typo fix style

--- src/components/ui/Graphs/Price/Price.tsx
+++ src/components/ui/Graphs/Price/Price.tsx
@@ -15,6 +15,7 @@ import { ThreeDots as Skeleton } from "@/components/ui/Skeleton";
 
 import { serializePrice } from "@/utils/index";
 import { optionsProducts } from "@/constant/common";
+
 import style from "./Price.module.css";
 
 ChartJS.register(
